6d44ea821ea7e13bc00ccdd03ab8dea5327bccce
nguyenvukhang <brew4k@gmail.com>
2021-08-04 16:40:40 +0800

    removed skip to content

--- components/Container.js
+++ components/Container.js
@@ -46,9 +46,6 @@ export default function Container(props) {
         )}
       </Head>
       <nav className="flex items-center justify-between w-full max-w-4xl p-8 mx-auto my-0 text-gray-900 bg-white sticky-nav md:my-8 dark:bg-black bg-opacity-60 dark:text-gray-100">
-        <a href="#skip" className="skip-nav">
-          Skip to content
-        </a>
         <button
           aria-label="Toggle Dark Mode"
           type="button"
