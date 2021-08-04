6b76ec99b9dbaff28c4e5aed237bf03e33d34682
nguyenvukhang <brew4k@gmail.com>
2021-08-05 00:45:25 +0800

    removed old components

--- components/MDXComponents.js
+++ components/MDXComponents.js
@@ -3,10 +3,6 @@ import Image from 'next/image';
 
 import ProsCard from '@/components/ProsCard';
 import ConsCard from '@/components/ConsCard';
-import Gumroad from '@/components/metrics/Gumroad';
-import Unsplash from '@/components/metrics/Unsplash';
-import Analytics from '@/components/metrics/Analytics';
-import YouTube from '@/components/metrics/Youtube';
 import Step from '@/components/Step';
 import ImageWithTheme from '@/components/ImageWithTheme';
 
@@ -29,13 +25,9 @@ const MDXComponents = {
   Image,
   ImageWithTheme,
   a: CustomLink,
-  Analytics,
   ConsCard,
-  Gumroad,
   ProsCard,
   Step,
-  Unsplash,
-  YouTube
 };
 
 export default MDXComponents;
