664096bc9dcfd6b60afe994adfe8bf0d1c3bc979
nguyenvukhang <brew4k@gmail.com>
2021-08-04 22:54:44 +0800

    moved Timeline closer to about

--- components/Timeline.js
+++ components/Timeline.js
@@ -73,7 +73,7 @@ export default function Timeline() {
 
   return (
     <>
-      <h3 className="font-bold text-2xl md:text-4xl tracking-tight mb-4 mt-8 text-black dark:text-white">
+      <h3 className="font-bold text-2xl md:text-4xl tracking-tight mb-4 mt-4 text-black dark:text-white">
         Timeline
       </h3>
       <Year>2021</Year>
