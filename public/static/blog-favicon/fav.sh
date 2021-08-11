59ddd5087a553067109d5a2eccb3d8d51dd2f6ab
nguyenvukhang <brew4k@gmail.com>
2021-08-11 15:42:55 +0800

    added raw favicons

---
+++ public/static/blog-favicon/fav.sh
@@ -0,0 +1,9 @@
+#! /bin/zsh
+T='/home/khang/repos/blog/public/static/favicons'
+cp android-icon-192x192.png $T/android-chrome-192x192.png
+cp favicon-16x16.png $T
+cp favicon-32x32.png $T
+cp favicon.ico $T
+cp apple-icon-180x180.png $T/apple-touch-icon.png
+cp ms-icon-150x150.png $T/mstile-150x150.png
+cp safari-pinned-tab.svg $T
