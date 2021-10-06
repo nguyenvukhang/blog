112cc5a7a1140c2bc0908b27419d469957d7143b
nguyenvukhang <brew4k@gmail.com>
2021-10-06 09:50:24 +0800

    navvy

---
+++ components/Navbar.js
@@ -0,0 +1,66 @@
+import { useTheme } from 'next-themes'
+import NextLink from 'next/link'
+
+function NavbarLink({ href, title }) {
+  const hover = "hover:bg-gray-200 rounded-lg"
+  return (
+    <NextLink href={href}>
+      <a className={"text-gray-900 sm:px-3 sm:py-2 sm:mr-2 dark:text-gray-100 " + hover}>{title}</a>
+    </NextLink>
+  )
+}
+
+function Navbar({ mounted }) {
+  const { resolvedTheme, setTheme } = useTheme()
+
+  function ToggleDarkModeButton() {
+    return (
+      <button
+        aria-label="Toggle Dark Mode"
+        type="button"
+        className="w-10 h-10 p-3 bg-gray-200 rounded dark:bg-gray-800"
+        onClick={() => setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')}
+      >
+        {mounted && (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            stroke="currentColor"
+            className="w-4 h-4 text-gray-800 dark:text-gray-200"
+          >
+            {resolvedTheme === 'dark' ? (
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+              />
+            ) : (
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+              />
+            )}
+          </svg>
+        )}
+      </button>
+    )
+  }
+
+  return (
+    <nav className="flex items-center justify-between w-full max-w-2xl pb-10 pt-8 mx-auto my-0 text-gray-900 md:my-8 dark:text-gray-100">
+      <div>
+        <NavbarLink href="/" title="Home" />
+        <NavbarLink href="/blog" title="Blog" />
+        <NavbarLink href="/photos" title="Photos" />
+        <NavbarLink href="/about" title="About" />
+      </div>
+      <ToggleDarkModeButton />
+    </nav>
+  )
+}
+
+export default Navbar
