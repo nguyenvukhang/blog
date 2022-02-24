b569e8bde1af48e3f8546ccaae8acb163c502782
nguyenvukhang <brew4k@gmail.com>
2022-02-24 11:58:54 +0800

    cleaned up next-12 conflicts

--- pages/api/top-tracks.js
+++ pages/api/top-tracks.js
@@ -10,10 +10,5 @@ export default async function handler(_, res) {
     title: track.name
   }))
 
-  res.setHeader(
-    'Cache-Control',
-    'public, s-maxage=86400, stale-while-revalidate=43200'
-  )
-
   return res.status(200).json({ tracks })
 }
