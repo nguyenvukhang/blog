5eddf19adf88780c3d6fba2fe8ab6adee32e4239
nguyenvukhang <brew4k@gmail.com>
2022-02-24 10:24:41 +0800

    removed google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,29 +1,14 @@
 import '@/styles/global.css'
-import { useEffect } from 'react'
-import { route } from 'next/dist/next-server/server/router'
-import { useRouter } from 'next/router'
-import ga from '@/lib/google-analytics'
 
 import { ThemeProvider } from 'next-themes'
 import { MDXProvider } from '@mdx-js/react'
 import MDXComponents from '@/components/MDXComponents'
 
 export default function App({ Component, pageProps }) {
-  const router = useRouter()
-
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-      ga.pageview(url)
-    }
-    router.events.on('routeChangeComplete', handleRouteChange)
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange)
-    }
-  }, [router.events])
   return (
     <ThemeProvider attribute="class">
       <MDXProvider components={MDXComponents}>
-        <Component {...pageProps} key={route} />
+        <Component {...pageProps} />
       </MDXProvider>
     </ThemeProvider>
   )
