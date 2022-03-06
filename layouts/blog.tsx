4ede8ac4e0cd72c13d4f35c43e9a7c6681e7273f
nguyenvukhang <brew4k@gmail.com>
2022-03-06 18:34:52 +0800

    refactor blog layout

--- layouts/blog.tsx
+++ layouts/blog.tsx
@@ -3,13 +3,38 @@ import { parseISO, format } from 'date-fns'
 
 import Container from '@/components/Container'
 
-function BlogTitle({ children }) {
-  return (
-    <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black dark:text-white">
-      {children}
-    </h1>
-  )
+type AvatarProps = {
+  size: number
 }
+
+const Avatar = ({ size }: AvatarProps) => (
+  <Image
+    alt="Khang"
+    height={size}
+    width={size}
+    src="/avatar.jpg"
+    className="rounded-full"
+  />
+)
+
+const ArticleDetails = ({ frontMatter }) => (
+  <div className="flex flex-col md:flex-row justify-between items-start md:items-center w-full mt-4 mb-8">
+    <div className="flex items-center">
+      <Avatar size={24} />
+      <p className="text-sm text-gray-700 dark:text-gray-300 ml-2">
+        {'khang / '}
+        {format(parseISO(frontMatter.publishedAt), 'MMM d, yyyy')}
+      </p>
+    </div>
+    <p className="text-sm text-gray-500 min-w-32 mt-2 md:mt-0">
+      {frontMatter.readingTime.text}
+    </p>
+  </div>
+)
+
+const ArticleContent = ({ children }) => (
+  <div className="prose dark:prose-dark">{children}</div>
+)
 
 export default function BlogLayout({ children, frontMatter }) {
   return (
@@ -19,31 +44,9 @@ export default function BlogLayout({ children, frontMatter }) {
       date={new Date(frontMatter.publishedAt).toISOString()}
       type="article"
     >
-      <article className="flex flex-col justify-center items-start max-w-2xl mx-auto mb-16 w-full">
-        <BlogTitle>{frontMatter.title}</BlogTitle>
-        <div className="flex flex-col md:flex-row justify-between items-start md:items-center w-full mt-2">
-          <div className="flex items-center">
-            <Image
-              alt="Khang"
-              height={24}
-              width={24}
-              src="/avatar.jpg"
-              className="rounded-full"
-            />
-            <p className="text-sm text-gray-700 dark:text-gray-300 ml-2">
-              {frontMatter.by}
-              {'Khang / '}
-              {format(parseISO(frontMatter.publishedAt), 'MMM d, yyyy')}
-            </p>
-          </div>
-          <p className="text-sm text-gray-500 min-w-32 mt-2 md:mt-0">
-            {frontMatter.readingTime.text}
-          </p>
-        </div>
-        <div className="prose dark:prose-dark max-w-none w-full">
-          {children}
-        </div>
-      </article>
+      <h1 className="mb-4">{frontMatter.title}</h1>
+      <ArticleDetails frontMatter={frontMatter} />
+      <ArticleContent>{children}</ArticleContent>
     </Container>
   )
 }
