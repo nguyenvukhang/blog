70f682c95bf74bcf0f23023c2b492f8774a6b6d4
nguyenvukhang <brew4k@gmail.com>
2022-03-21 15:16:03 +0800

    ga second try

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -24,16 +24,15 @@ export default function App({ Component, pageProps }) {
       <MDXProvider components={MDXComponents}>
         <Script
           strategy="lazyOnload"
-          src={`https://www.googletagmanager.com/gtag/js?id=G-YLMYNMR5FM`}
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
         />
-        <Script
-          strategy="lazyOnload"
-          src={`
+        <Script strategy="lazyOnload">
+          {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'G-YLMYNMR5FM');`}
-        />
+            gtag('config', ${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS});`}
+        </Script>
 
         <Component {...pageProps} />
       </MDXProvider>
