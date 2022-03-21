cce7a24cd81f65ea7c267022ae94c3f91810f932
nguyenvukhang <brew4k@gmail.com>
2022-03-21 16:36:43 +0800

    add firebase anon auth

---
+++ lib/firebase.ts
@@ -0,0 +1,43 @@
+import { initializeApp, getApp, getApps } from 'firebase/app'
+import { getAuth, signInAnonymously } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_APIKEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTHDOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECTID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APPID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENTID
+}
+
+// Initialize Firebase
+let app: any
+if (getApps.length === 0) {
+  app = initializeApp(firebaseConfig)
+  console.log('firebase: initialized')
+} else {
+  console.log('firebase: continued')
+  app = getApp()
+}
+
+const auth = getAuth(app)
+let anon: any
+signInAnonymously(auth)
+  .then((a) => {
+    console.log('successful anonymous signin')
+    console.log(a)
+    anon = a
+  })
+  .catch((error) => {
+    const errorCode = error.code
+    const errorMessage = error.message
+    console.log(errorCode, errorMessage)
+  })
+const db = getFirestore(app)
+
+export { auth, db, anon }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,10 @@ import '@/styles/global.css'
 import { ThemeProvider } from 'next-themes'
 import { MDXProvider } from '@mdx-js/react'
 import MDXComponents from '@/components/MDXComponents'
+import { anon } from '@/lib/firebase'
 
 export default function App({ Component, pageProps }) {
+  console.log(anon)
   return (
     <ThemeProvider attribute="class">
       <MDXProvider components={MDXComponents}>
