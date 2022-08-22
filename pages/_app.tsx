64bf74469b5aa32ce6fc8ecffc670106f028ff9a
nguyenvukhang <brew4k@gmail.com>
2022-08-22 21:32:02 +0800

    remove firebase

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,20 +3,8 @@ import '@/styles/global.css'
 import { ThemeProvider } from 'next-themes'
 import { MDXProvider } from '@mdx-js/react'
 import MDXComponents from '@/components/MDXComponents'
-import { anon, db } from '@/lib/firebase'
-import { arrayUnion, updateDoc, Timestamp, doc } from 'firebase/firestore'
-import { useEffect } from 'react'
 
 export default function App({ Component, pageProps }) {
-  const myDoc = doc(db, 'analytics', 'views')
-  const ts = Timestamp.now()
-
-  useEffect(() => {
-    updateDoc(myDoc, {
-      timestamp: arrayUnion(ts)
-    })
-  }, [])
-
   return (
     <ThemeProvider attribute="class">
       <MDXProvider components={MDXComponents}>
